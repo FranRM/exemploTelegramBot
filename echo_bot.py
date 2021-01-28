81f6377d3631e0b0d69dc6f06594b59bf562e931
FranRM <32239030+FranRM@users.noreply.github.com>
2021-01-28 17:22:49 +0100

    cambio ids

--- echo_bot.py
+++ echo_bot.py
@@ -10,34 +10,34 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['chat'])
 def send_pepitas(message):
-	bot.send_message("217250736","Hola @Femio23")
+	bot.send_message("idchat","Hola @Femio23")
 
 @bot.message_handler(commands=['david'])
 def send_pepitas(message):
-	bot.send_message("25216209","Habeeere tipoooooooooo.")
+	bot.send_message("idchat","Habeeere tipoooooooooo.")
 
 @bot.message_handler(commands=['pavel'])
 def send_pepitas(message):
-	bot.send_message("148714029","Hola Pavel")
+	bot.send_message("idchat","Hola Pavel")
 
 @bot.message_handler(commands=['group'])
 def send_pepitas(message):
-	bot.send_message("-578948572","Hola @Femio23 en GrupoBot")
+	bot.send_message("idchat","Hola @Femio23 en GrupoBot")
 
 # Función de reenvio de los mensajes al propietario de todos los mensajes recibidos por el bot.
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	nome=message.from_user.username
-	if (message.from_user.id == 148714029):
+	if (message.from_user.id == idchat):
 		nome= "Pavel"
-	elif (message.from_user.id == 217250736):
+	elif (message.from_user.id == idchat):
 		nome= "Fran"
-	elif (message.from_user.id == 25216209):
+	elif (message.from_user.id == idchat):
 		nome= "David"
-		elif (message.from_user.id == 141747937):
+		elif (message.from_user.id == idchat):
 		nome= "Ivan Libreinnova"
 	else:
 		nome = "alguen (" + str(message.from_user.id) +")"
-	bot.send_message(217250736,"Hola @Femio23, "+ nome + " díxome que '" + message.text+ "'.")
+	bot.send_message(idchat,"Hola @Femio23, "+ nome + " díxome que '" + message.text+ "'.")
 
 bot.polling()
